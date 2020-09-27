787d71e58093176d136843320fc9368bfe5d0fe2
TrollTown <williamyinanshen2009@gmail.com>
2020-09-27 20:30:35 +1000

    added makefile

---
+++ Makefile
@@ -0,0 +1,13 @@
+# Makefile for COMP9243 Distributed Shared Memory Assignment
+
+CC = gcc
+CFLAGS = -Wall -Werror -std=gnu99 -g
+AR = ar
+
+all : dsm
+
+dsm : dsm.c
+	$(CC) -o dsm dsm.c
+
+clean :
+	rm -f *.o dsm
--- dsm.c
+++ dsm.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 // #include "sm.h"
+
+#define MAX_HOSTNAME_LENGTH 255
+#define MAX_ARGUMENT_LENGTH 256
+#define MAX_HOSTS 10
+
+void read_hostnames_from_file(char * filename, char **hostnames);
 
 int main (int argc, char **argv) {
     
@@ -23,16 +30,21 @@ int main (int argc, char **argv) {
     extern int optind;
     int opt = 0;
     const char *optstring = "H:hl:n:v";
-    char *hostfile_name;
+    char *hostfile_name = NULL;
+    char **hostnames = NULL;
+    int hostnames_set_flag = 0;
     char *logfile_name;
     int n_node_processes;
     char *executable_filename;
     char **node_options;
+
+    // Use getopt to retrieve command line options
     while ((opt = getopt(argc, argv, optstring)) != -1) {
         switch(opt) {
             case 'H':
                 hostfile_name = optarg;
                 printf("hostfile: %s\n", hostfile_name);
+                read_hostnames_from_file(hostfile_name, hostnames);
                 break;
             case 'h':
                 printf("%s\n", usage);
@@ -55,8 +67,6 @@ int main (int argc, char **argv) {
         }
         
     }
-    // printf("optind: %d\n", optind);
-    // printf("argc: %d\n", argc);
 
     // Get executable filename
     if (optind < argc) {
@@ -66,19 +76,59 @@ int main (int argc, char **argv) {
     } else {
         printf("error: Missing argument EXECUTABLE-FILE\n");
         printf("%s\n", usage);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     // Collect node options
     if (optind < argc) {
         int length = argc - optind;
-        node_options = malloc(sizeof(char) * 128 * length);
+        node_options = malloc(sizeof(char*) * length);
+        for (int i = 0; i < length; i++) {
+            node_options[i] = malloc(sizeof(char) * MAX_ARGUMENT_LENGTH);
+        }
         printf("Node arguments:\n");
         for (int i = 0; i < length; i++) {
-            node_options[i] = argv[optind];
+            strcpy(node_options[i], argv[optind]);
             printf("%s\n", node_options[i]);
             optind++;
         }
     }
+
+    // If hostnames are not set (-H flag was not used)
+    if (hostnames == NULL) {
+        // Read from 'hosts' instead
+        read_hostnames_from_file("hosts", hostnames);
+    }
     return 0;
+}
+
+/* Reads hostnames from a file into an array of strings*/
+void read_hostnames_from_file(char * filename, char **hostnames) {
+    hostnames = malloc(sizeof(char*) * MAX_HOSTS);
+    for (int i = 0; i < MAX_HOSTS; i++) {
+        hostnames[i] = malloc(sizeof(char) * MAX_HOSTNAME_LENGTH);
+    }
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("error: unable to open file: %s\n", filename);
+        exit(EXIT_FAILURE);
+    } 
+    // Read hostnames from the file using fgets
+    char hostname[MAX_HOSTNAME_LENGTH];
+    int i = 0;
+    while (fgets(hostname, MAX_HOSTNAME_LENGTH, fp) != NULL) {
+        hostname[strcspn(hostname, "\n")] = 0; // strip newline
+        printf("hostname: %s\n", hostname);
+        strcpy(hostnames[i], hostname);
+        i++;
+    }
+    fclose(fp);
+
+    /* DEBUG */
+    // Check hostnames
+    printf("Hostnames:\n");
+    for (int i = 0; i < MAX_HOSTS; i++) {
+        printf("%s\n", hostnames[i]);
+    }
+    /* END DEBUG */
 }
