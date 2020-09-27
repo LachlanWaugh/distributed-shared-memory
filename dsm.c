8fcd6c1a183c947ba822856a574eb3e50d433d49
TrollTown <williamyinanshen2009@gmail.com>
2020-09-28 02:45:24 +1000

    progress

--- dsm.c
+++ dsm.c
@@ -8,9 +8,10 @@
 
 #define MAX_HOSTNAME_LENGTH 255
 #define MAX_ARGUMENT_LENGTH 256
+#define MAX_COMMAND_LENGTH 2048
 #define MAX_HOSTS 64
 
-void read_hostnames_from_file(char * filename, char **hostnames, int *n_hostnames);
+int read_hostnames_from_file(char * filename, char **hostnames);
 
 int main (int argc, char **argv) {
     
@@ -33,8 +34,8 @@ int main (int argc, char **argv) {
     const char *optstring = "H:hl:n:v";
     char *hostfile_name = NULL;
     // Create hostnames array and initialise to NULL
-    int n_hostnames = 1;
-    char **hostnames = NULL;
+    int n_hostnames;
+    char **hostnames = malloc(sizeof(char*) * MAX_HOSTS);
     char *logfile_name = NULL;
     int n_node_processes = 1; // default to 1
     char *executable_filename;
@@ -46,7 +47,7 @@ int main (int argc, char **argv) {
             case 'H':
                 strcpy(hostfile_name, optarg);
                 printf("hostfile: %s\n", hostfile_name);
-                read_hostnames_from_file(hostfile_name, hostnames, &n_hostnames);
+                n_hostnames = read_hostnames_from_file(hostfile_name, hostnames);
                 break;
             case 'h':
                 printf("%s\n", usage);
@@ -67,7 +68,6 @@ int main (int argc, char **argv) {
                 break;
         }
     }
-
     // Get executable filename
     if (optind < argc) {
         executable_filename = argv[optind];
@@ -77,7 +77,7 @@ int main (int argc, char **argv) {
         printf("error: Missing mandatory argument EXECUTABLE-FILE\n");
         exit(EXIT_FAILURE);
     }
-
+    
     // Collect node options
     if (optind < argc) {
         node_options_length = argc - optind;
@@ -92,42 +92,55 @@ int main (int argc, char **argv) {
             optind++;
         }
     }
-
     // If hostnames are not set (-H flag was not used)
     if (hostnames[0] == NULL) {
         // Read from 'hosts' instead
-        read_hostnames_from_file("hosts", hostnames, n_hostnames);    
+        n_hostnames = read_hostnames_from_file("hosts", hostnames);
     }
 
     // Fork helper processes
     int helper_proc_count = 0;
     pid_t proc_id;
     while (helper_proc_count < n_node_processes) {
-        int allocated_hostname_index;
-        if (helper_proc_count < n_hostnames) {
-            allocated_hostname_index = helper_proc_count;
-        }
-        else {
-            allocated_hostname_index = helper_proc_count % n_hostnames;
-        }
         proc_id = fork();
         helper_proc_count++;
         // Parent process
         if (proc_id > 0) {
             printf("Parent [%d]: successfully forked %d\n", getpid(), proc_id);
             // Wait for child processes
-            for (int i = 0; i < helper_proc_count; i++) {
-                int exited_id = wait(NULL);
-                printf("process exited with id: %d\n", exited_id);
+            int status;
+            int exited_id = wait(&status);
+            if (exited_id == -1) {
+                printf("error: wait\n");
+            }
+            if (WIFEXITED(status)) {
+                printf("Process [%d] exited with status %d\n", exited_id, WEXITSTATUS(status));
+            } 
+            else if (WIFSIGNALED(status)) {
+                printf("Process [%d] killed by signal %d\n", exited_id, WTERMSIG(status));
+            } 
+            else if (WIFSTOPPED(status)) {
+                printf("Process [%d] stopped by signal %d\n", exited_id, WSTOPSIG(status));
+            } 
+            else if (WIFCONTINUED(status)) {
+                printf("Process [%d] continued\n", exited_id);
             }
         } 
         // Child process
         else if (proc_id == 0) {
             printf("This is child process [%d]\n", getpid());
+            int allocated_hostname_index;
+            if (helper_proc_count < n_hostnames) {
+                allocated_hostname_index = helper_proc_count - 1;
+            }
+            else {
+                allocated_hostname_index = (helper_proc_count - 1) % n_hostnames;
+            }
             printf("attempting to run node process on: %s\n", hostnames[allocated_hostname_index]);
-            char *node_proc_command;
-            sprintf(node_proc_command, "ssh %s ~/comp9243/distributed-shared-memory/hello", hostnames[allocated_hostname_index]);
-            int err = system(node_proc_command);
+            char node_process_command[MAX_COMMAND_LENGTH];
+            int sprintf_status = sprintf(node_process_command, "ssh %s %s", hostnames[allocated_hostname_index], executable_filename);
+            printf("sprintf_status:%d\n", sprintf_status);
+            int err = system(node_process_command);
             printf("err: %d\n", err);
             exit(EXIT_SUCCESS);
         }
@@ -147,23 +160,30 @@ int main (int argc, char **argv) {
         }
         free(node_options);
     }
-    
+    if (hostnames == NULL) {
+        printf("hostnames is null\n");
+    }
     for (int i = 0; i < MAX_HOSTS; i++) {
-        free(hostnames[i]);
+        if (hostnames[i] != NULL) {
+            free(hostnames[i]);
+        }
+        else {
+            break;
+        }
     }
     free(hostnames);
 
     return 0;
 }
 
-/* Reads hostnames from a file into an array of strings */
-void read_hostnames_from_file(char * filename, char **hostnames, int *n_hostnames) {
-    hostnames = malloc(sizeof(char*) * MAX_HOSTS);
+/* Reads hostnames from a file into an array of strings and returns the number of hostnames */
+int read_hostnames_from_file(char * filename, char **hostnames) {
     FILE *fp = fopen(filename, "r");
     if (fp == NULL) {
         // If file cannot be opened, use localhost
         hostnames[0] = malloc(sizeof(char) * MAX_HOSTNAME_LENGTH);
         strcpy(hostnames[0], "localhost");
+        return 1;
     } 
     else {
         // Read hostnames from the file using fgets
@@ -176,20 +196,13 @@ void read_hostnames_from_file(char * filename, char **hostnames, int *n_hostname
             strcpy(hostnames[i], hostname);
             i++;
         }
+        fclose(fp);
         // If file is empty use localhost
         if (i == 0) {
             hostnames[0] = malloc(sizeof(char) * MAX_HOSTNAME_LENGTH);
             strcpy(hostnames[0], "localhost");
+            return 1;
         }
-        fclose(fp);
+        return i;
     }
-    /* DEBUG */
-    // Print hostnames
-    printf("Hostnames:\n");
-    for (int i = 0; i < MAX_HOSTS; i++) {
-        if (hostnames[i] != NULL) {
-            printf("%s\n", hostnames[i]);
-        }
-    }
-    /* END DEBUG */
 }
